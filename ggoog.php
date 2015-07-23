4fe72f82bcf86023401a99b01a60ba9f15127a7e
3Turr <a.x.l@live.com>
2015-07-23 14:27:36 -0700

    Copyright
    Copyright

--- ggoog.php
+++ ggoog.php
@@ -1,6 +1,22 @@
 <?php
 //BY 3Turr
-
+/*
+Copyright {2015} {Ahmed Alarbi}
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+Don't delete this, respect the coder who shared it for free.
+Title: 3Turr ~ CPanel Cracker
+By: 3Turr
+Contact: facebook.com/ahmed.f.alarbi
+note: This script is the fastest cracker for CPanels you can run.
+*/
 include_once('./functions.php');
 include_once('./agents.php');
 //## Functions
